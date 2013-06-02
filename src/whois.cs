6e0e9511874a9f9d81daf9a38bc66b691a18b9a2
Matthew Edwards <m.edwards7@lancs.ac.uk>
2013-06-02 02:17:13 +0100

    Added Makefile for development under Mono, tweaked configuration.
    Changes:
    
    1) Tweaked code to accept parameters for the location of the Tor executable and
    the MySql database connection string, making the system more portable. Might
    also have added some debug print statements, those can be removed.
    
    2) Added makefile which eases compilation and running under Mono. Might need
    tweaking for others, but should provide a working template. Didn't touch any of
    the VS config stuff.
    
    3) Renamed source folder to src.
    
    -----
    balan

---
+++ src/whois.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
+
+using Starksoft.Net.Proxy;
+
+namespace whore
+{
+    class WhoisTask : TorTask
+    {
+        public readonly string Host;
+        const int chunk_size = 65535;
+        byte[] chunk = new byte[chunk_size];
+        private StringBuilder buffer = new StringBuilder();
+
+        private string getTLD()
+        {
+            return Host.Substring(Host.LastIndexOf(".") + 1);
+        }
+        public WhoisTask(string host)
+        {
+            Host = host;
+        }
+        public WhoisTask(IPAddress ip)
+        {
+            StringBuilder host = new StringBuilder();
+            switch (ip.AddressFamily)
+            {
+                // Turn the address into its in-addr.arpa form.
+                case System.Net.Sockets.AddressFamily.InterNetwork:
+                    foreach (byte octet in ip.GetAddressBytes().Reverse())
+                    {
+                        host.Append(octet.ToString() + ".");
+                    }
+                    host.Append("in-addr.arpa");
+                    break;
+                // Turn the address into its ip6.arpa form.
+                case System.Net.Sockets.AddressFamily.InterNetworkV6:
+                    foreach (byte octet in ip.GetAddressBytes().Reverse())
+                    {
+                        string hex = string.Format("{0:x2}", octet);
+                        host.Append(string.Format("{0}.{1}.", hex[1], hex[0]));
+                    }
+                    host.Append("ip6.arpa");
+                    break;
+
+            }
+            Host = host.ToString();
+        }
+        protected void onRecieve(IAsyncResult result)
+        {
+            int nBytesRec = tor.proxy.TcpClient.Client.EndReceive(result);
+            // If no bytes were received, the connection should be closing down.
+            if (nBytesRec <= 0)
+            {
+                tor.proxy.TcpClient.Close();
+                tor.State = TorInstance.TorState.Ready;
+                if (callback != null)
+                    callback(buffer.ToString());
+            }
+            else
+            {
+                buffer.Append(Encoding.ASCII.GetString(chunk));
+                tor.proxy.TcpClient.Client.BeginReceive(chunk, 0, chunk.Length, SocketFlags.None, new AsyncCallback(onRecieve), this);
+            }
+        }
+        public override void onConnect(object sender, CreateConnectionAsyncCompletedEventArgs e)
+        {
+            base.onConnect(sender, e);
+            if (e.Error == null)
+            {
+                TcpClient tcp = e.ProxyConnection;
+                tcp.Client.Send(ASCIIEncoding.ASCII.GetBytes(Host + "\r\n"));
+                tcp.Client.BeginReceive(chunk, 0, chunk.Length, SocketFlags.None, new AsyncCallback(this.onRecieve), this);
+            }
+            else
+            {
+                Console.WriteLine("Socket error has occured. " + e.Error);
+            }
+        }
+        public void dnsCallback(object result)
+        {
+            DnsTransaction.ResourceRecord rr = result as DnsTransaction.ResourceRecord;
+            IPAddress addr = new IPAddress(rr.data);
+            try
+            {
+                tor.proxy.CreateConnectionAsyncCompleted += new EventHandler<CreateConnectionAsyncCompletedEventArgs>(onConnect);
+                tor.proxy.CreateConnectionAsync(addr.ToString(), 43);
+            }
+            catch (SocketException e)
+            {
+                Debug.WriteLine(e.ToString());
+            }
+        }
+
+        public override void execute(TorInstance torInstance, TaskResultHandler callback)
+        {
+            base.execute(torInstance, callback);
+            // doesn't seem like the appropriate level to be spawning tasks... for instance, how to interface db?
+            // spwans a DnsTask to resolve the CNAME that tells us where to connect to perform the whois. 
+            new DnsTask(new DnsTransaction.QuestionRecord(string.Format("{0}.whois-servers.net", getTLD()), DnsTransaction.QTYPE.CNAME, DnsTransaction.RCLASS.IN)).execute(torInstance, callback);
+        }
+    }
+}
---
+++ src/whore.cs
@@ -0,0 +1,143 @@
+﻿//TODO: Save/cache the results, filtering unnesc work. (And implement root-hints).
+//TODO: Tasks should be able to Async create other tasks ("chains of tasks" too?)
+//TODO: Error handling. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Net.Sockets;
+using Starksoft.Net.Proxy;
+using System.IO;
+
+namespace whore
+{
+    class Whore
+    {
+        DB db;
+
+        public Queue<Task> taskList = new Queue<Task>();
+        public List<TorInstance> torInstances = new List<TorInstance>();
+
+        public Whore(DB _db, string torloc, int basePort = 9049, byte threads = 1)
+        {
+            db = _db;
+            // Create data subdirectory if it doesn't already exist. 
+            if (!System.IO.Directory.Exists("./data"))
+            {
+                try
+                {
+                    System.IO.Directory.CreateDirectory("./data");
+                }
+                catch
+                {
+                    Console.WriteLine("Failed to create data directory.");
+                }
+            }
+
+            // Create TorInstances
+            for (int i = 1; i <= threads; i++)
+            {
+                int controlPort = basePort - i;
+                int socksPort = basePort + i;
+                // useExisting?
+                torInstances.Add(new TorInstance(torloc, controlPort, socksPort, true));
+            }
+        }
+
+        TorInstance getAvailableTorInstance()
+        {
+            while (true)
+            {
+                foreach (TorInstance torInstance in torInstances)
+                {
+                    switch (torInstance.State)
+                    {
+                        case TorInstance.TorState.Ready:
+//                            torInstance.StartTorThread();
+                            return torInstance;
+                        case TorInstance.TorState.Terminated:
+                            torInstance.StartTorThread();
+                            Debug.WriteLine("Restarting terminated TorInstance({0})", torInstance.GetHashCode());
+                            goto default;
+                        default:
+                        case TorInstance.TorState.Busy:
+                        case TorInstance.TorState.Bootstrapping:
+                        case TorInstance.TorState.Error:
+                            continue;
+                    }
+                }
+                Thread.Sleep(25);
+            }
+
+        }
+
+        public void doWork()
+        {
+            if (taskList.Count > 0)
+            {
+                Task task = taskList.Dequeue();
+                if (task is TorTask)
+                {
+                    TorTask torTask = (TorTask)task;
+                    TorInstance torInstance = getAvailableTorInstance();
+                    Task.TaskResultHandler a, b, c;
+                    a = displayResults;
+                    b = db.cache;
+                    c = a + b;
+
+                    torTask.execute(torInstance,c);
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("No work to be done.");
+                Thread.Sleep(50);
+            }
+
+        }
+        public void displayResults(object buffer)
+        {
+            System.Console.WriteLine(buffer);
+        }
+        public void queue(Task task)
+        {
+            taskList.Enqueue(task);
+        }
+    }
+
+
+    class MainLoop
+    {
+        static int Main(string[] args)
+        {
+	    if(args.Length < 2){
+		System.Console.WriteLine("You should provide:\n\t1) The database connection string\n\t2) The location of your Tor executable");
+		System.Environment.Exit(1);
+	    }
+	    string dbstring = args[0];
+	    string torloc = args[1];
+	    System.Console.WriteLine(string.Format("DBSTRING: {0} | TORLOC: {1}", dbstring, torloc));
+            DB db = new DB(dbstring);
+
+            Whore whore = new Whore(db, torloc);
+            //  Dns.readRootHints();
+            
+            #region StaticTasks
+            // Assign some tasks
+            whore.queue(new WhoisTask("example.com"));
+            whore.queue(new DnsTask(new DnsTransaction.QuestionRecord("example.com", DnsTransaction.QTYPE.A, DnsTransaction.RCLASS.IN)));
+            #endregion
+
+            // Main loop
+            while (true)
+            {
+                whore.doWork();
+            }
+        }
+    }
+}
